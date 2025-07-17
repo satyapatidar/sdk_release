444ca6c80318741ef821941e998a58a121c1e8d0
Satya Patidar <satya.p@ccube.com>
2025-07-17 13:18:51 +0530

    fix: added version rc

---
+++ .versionrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  types: [
+    { type: 'feat', section: 'Features' },
+    { type: 'fix', section: 'Bug Fixes' },
+    { type: 'chore', section: 'Chores', hidden: false },
+    { type: 'docs', section: 'Docs', hidden: false },
+    { type: 'test', section: 'Tests', hidden: false },
+    { type: 'refactor', section: 'Refactors', hidden: false }
+  ]
+};
